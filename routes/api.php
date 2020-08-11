c23ea7998ca9c1d980460abd6e84af03492b2369
shinder <shinder.lin@gmail.com>
2020-08-12 00:25:25 +0800

    測試 api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('try-api', function(Request $request){
+    return [
+        'url' => $request->url(),
+        'method' => $request->getMethod()
+    ];
+});
+
+Route::post('try-api', function(Request $request){
+    return [
+        'url' => $request->url(),
+        'method' => $request->getMethod()
+    ];
+});
