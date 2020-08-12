be3d533239eca94b563961982e9ee1c3836c2de1
Shinder Lin <shinder.lin@gmail.com>
2020-08-12 15:58:29 +0800

    tools 02

---
+++ tools/test02.php
@@ -0,0 +1,8 @@
+<?php
+require '__connect_db.php';
+
+//$sql ="SELECT `客戶編號`, `地址` FROM `customers` LIMIT 10 ";
+//
+//$rows = $pdo->query($sql)->fetchAll();
+//
+//echo json_encode($rows, JSON_UNESCAPED_UNICODE);
