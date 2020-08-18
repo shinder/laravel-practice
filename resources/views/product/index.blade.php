71bf4ef2a6d6532c2895bb283db3342701835ff1
Shinder Lin <shinder.lin@gmail.com>
2020-08-18 10:31:40 +0800

    product.index 列表

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,6 +2,37 @@
 
 @section('content')
 
-    <h2>Hello</h2>
-    @parent
+    <div class="row">
+        <div class="col">
+            {{ $products->links() }}
+        </div>
+    </div>
+
+    <!-- `商品編號`, `品名`, `分類`, `材質`, `面漆`, `售價` -->
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th scope="col">商品編號</th>
+            <th scope="col">品名</th>
+            <th scope="col">材質</th>
+            <th scope="col">面漆</th>
+            <th scope="col">售價</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($products as $p)
+        <tr>
+            <td>{{ $p->商品編號 }}</td>
+            <td>{{ $p->品名 }}</td>
+            <td>{{ $p->材質 }}</td>
+            <td>{{ $p->面漆 }}</td>
+            <td>{{ $p->售價 }}</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+
+
+
 @endsection
